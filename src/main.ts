9770c4c010fbe1324ab35b25edb28edbf670e8e6
through-your-tears <warstream55@gmail.com>
2022-04-16 01:03:28 +0300

    some fixes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import App from './App.svelte';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.js';
-// import 'bootstrap';
 
 const app = new App({
 	target: document.body,
