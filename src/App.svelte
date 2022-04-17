e33602c333f43b90c38deb25f3f7611b1c7bcea7
Dmitri <warstream55@gmail.com>
2022-04-17 14:19:54 +0300

    refactoctored, now using each instead of copy-paste html code

--- src/App.svelte
+++ src/App.svelte
@@ -3,60 +3,57 @@
 	import Gold from "./Gold.svelte";
 	import Swann from "./Swann.svelte";
 	import Fibonacci from "./Fibonacci.svelte";
+	const methods: object[] = [
+		{
+			"id": "One",
+			"component": Swann,
+			"name": "Метод Свена",
+			"passed": false
+		},
+		{
+			"id": "Two",
+			"component": Dihotomiya,
+			"name": "Метод дихотомии",
+			"passed": true
+		},
+		{
+			"id": "Three",
+			"component": Fibonacci,
+			"name": "Метод Фибоначчи",
+			"passed": false
+		},
+		{
+			"id": "Four",
+			"component": Gold,
+			"name": "Метод золотого сечения",
+			"passed": true
+		},
+	]
 </script>
 
 <main>
 	<h1>Методы оптимизации!</h1>
 	<div class="accordion" id="accordionExample">
-		<div class="accordion-item">
-			<h2 class="accordion-header" id="headingOne">
-				<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-					Задание 1(метод Свена)
-				</button>
-
-			</h2>
-			<div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-				<div class="accordion-body">
-					<Swann/>
+		{#each methods as method(method.id)}
+			<div class="accordion-item">
+				<h2 class="accordion-header" id="heading{method.id}">
+					{#if method.passed}
+						<button class="accordion-button bg-success text-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{method.id}" aria-expanded="true" aria-controls="collapse{method.id}">
+							{method.name}
+						</button>
+					{:else}
+						<button class="accordion-button bg-danger text-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{method.id}" aria-expanded="true" aria-controls="collapse{method.id}">
+							{method.name}
+						</button>
+					{/if}
+				</h2>
+				<div id="collapse{method.id}" class="accordion-collapse collapse show" aria-labelledby="heading{method.id}" data-bs-parent="#accordionExample">
+					<div class="accordion-body">
+						<svelte:component this={method.component}/>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="accordion-item">
-			<h2 class="accordion-header" id="headingTwo">
-				<button class="accordion-button bg-success text-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-					Задание 2(метод дихотомии)
-				</button>
-			</h2>
-			<div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-				<div class="accordion-body">
-					<Dihotomiya/>
-				</div>
-			</div>
-		</div>
-		<div class="accordion-item">
-			<h2 class="accordion-header" id="headingThree">
-				<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
-					Задание 2(метод Фибоначчи)
-				</button>
-			</h2>
-			<div id="collapseThree" class="accordion-collapse collapse show" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-				<div class="accordion-body">
-					<Fibonacci/>
-				</div>
-			</div>
-		</div>
-		<div class="accordion-item">
-			<h2 class="accordion-header" id="headingFour">
-				<button class="accordion-button bg-success text-light" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
-					Задание 2(метод золотого сечения)
-				</button>
-			</h2>
-			<div id="collapseFour" class="accordion-collapse collapse show" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
-				<div class="accordion-body">
-					<Gold/>
-				</div>
-			</div>
-		</div>
+		{/each}
 	</div>
 </main>
 
