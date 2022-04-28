ad2d16712c56e31dd6cedfad3804298591bac853
Dmitri <warstream55@gmail.com>
2022-04-29 00:37:02 +0300

    Swann method done

---
+++ src/Swann.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+    function f(x: number): number {
+        return x * x - 4 * x + 7;
+    }
+    function random (min: number, max: number): number {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    function swann(fun: (x: number) => number, eps: number): any {
+        let x: number = random(1, 100);
+        let f0: number = f(x - eps);
+        let f1: number = f(x);
+        let f2: number = f(x + eps);
+        let i: number = 0;
+        while (f0 > f1 < f2 && i < 100) {
+            f0= f(x - eps);
+            f1 = f(x);
+            f2= f(x + eps);
+            i++;
+        }
+        if(i == 100) {
+            return "Не удаётся найти решение"
+        }
+        if(f0 >= f1 <= f2) {
+            return [f0, f2];
+        } else {
+            let a: number;
+            let b: number;
+            let delta: number;
+            let k: number = 1;
+            if(f0 >= f1 >= f2) {
+                delta = eps;
+                a = x;
+                x += eps;
+            }
+            else if(f0 <= f1 <= f2) {
+                delta = eps * -1;
+                b = x;
+                x += eps;
+            }
+            let xk = x + Math.pow(2, k) * delta;
+            while (f(xk) < f(x)) {
+                if (delta == eps) {
+                    a = x;
+                }
+                else {
+                    b = x;
+                }
+                x = xk;
+                xk = x + Math.pow(2, k) * delta;
+                k++;
+            }
+            if (delta == eps) {
+                b = xk;
+            }
+            else {
+                a = xk;
+            }
+            return [a, b];
+        }
+    }
+    let ans: number[] = swann(f, 0.5);
+</script>
+
+<!--{#each print as p}-->
+<!--    <h3>Шаг {@html p.id}</h3>-->
+<!--    <p>{@html p.ans}</p>-->
+<!--{/each}-->
+<h3>Ответ: x = {ans}</h3>
